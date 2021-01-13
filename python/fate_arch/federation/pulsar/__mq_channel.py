58323e28c69989123e8318fec50944be178ae475
jiahaoc <jiahaoc@vmware.com>
2021-01-13 13:30:28 +0800

    temporary commit
    Signed-off-by: jiahaoc <jiahaoc@vmware.com>

--- python/fate_arch/federation/pulsar/__mq_channel.py
+++ python/fate_arch/federation/pulsar/__mq_channel.py
@@ -4,11 +4,10 @@
 ########################################################
 
 import pika
-import time 
+import time
 from fate_arch.common import log
 
 LOGGER = log.getLogger()
-
 
 def connection_retry(func):
     """retry connection
@@ -27,52 +26,50 @@ def connection_retry(func):
         return res 
     return wrapper
 
-
 class MQChannel(object):
-
-    def __init__(self, host, port, user, password, vhost, send_queue_name, receive_queue_name, party_id, role, extra_args: dict):
+    # TODO add credential to secure pulsar cluster 
+    def __init__(self, host, port, pulsar_namespace, pulsar_topic, party_id, role, credential=None, extra_args: dict = None):
         self._host = host
         self._port = port
-        self._credentials = pika.PlainCredentials(user, password)
-        self._vhost = vhost
-        self._send_queue_name = send_queue_name
-        self._receive_queue_name = receive_queue_name
+        self._namespace = pulsar_namespace
+        self._topic = pulsar_topic
+        self._credential = credential
         self._conn = None
         self._channel = None
         self._party_id = party_id
         self._role = role
         self._extra_args = extra_args
-        
+
         if "heartbeat" not in self._extra_args:
             self._extra_args["heartbeat"] = 3600
-       
+
     @property
     def party_id(self):
         return self._party_id
-    
+
     @connection_retry
     def basic_publish(self, body, properties):
         self._get_channel()
         LOGGER.debug(f"send queue: {self._send_queue_name}")
         return self._channel.basic_publish(exchange='', routing_key=self._send_queue_name, body=body,
                                            properties=properties)
-        
-    @connection_retry   
+
+    @connection_retry
     def consume(self):
         self._get_channel()
         LOGGER.debug(f"receive queue: {self._receive_queue_name}")
-        return self._channel.consume(queue=self._receive_queue_name) 
-           
+        return self._channel.consume(queue=self._receive_queue_name)
+
     @connection_retry
     def basic_ack(self, delivery_tag):
         self._get_channel()
         return self._channel.basic_ack(delivery_tag=delivery_tag)
-    
-    @connection_retry    
+
+    @connection_retry
     def cancel(self):
         self._get_channel()
-        return self._channel.cancel() 
-    
+        return self._channel.cancel()
+
     @connection_retry
     def _get_channel(self):
         if self._check_alive():
@@ -81,14 +78,10 @@ class MQChannel(object):
             self._clear()
 
         if not self._conn:
-            self._conn = pika.BlockingConnection(pika.ConnectionParameters(host=self._host, port=self._port,
-                                                                           virtual_host=self._vhost,
-                                                                           credentials=self._credentials,
-                                                                           **self._extra_args))
+            self._conn = pulsar.Client('pulsar://{}:{}'.format(self._host, self._port))
 
         if not self._channel:
-            self._channel = self._conn.channel()
-            self._channel.confirm_delivery()             
+            producer
 
     def _clear(self):
         try:
